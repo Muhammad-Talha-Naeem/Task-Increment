c0915861e30d6824f8dc814c00f5984eee0c8cce
Muhammad-Talha-Naeem <75784944+Muhammad-Talha-Naeem@users.noreply.github.com>
2021-07-13 13:52:10 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <inc/>
+  <inc/>
+  <inc/>
+</template>
+
+<script>
+import inc from './components/inc.vue'
+export default {
+  name: 'App',
+  components: {
+    inc,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/inc.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 >{{a}}</h1>
+        <button v-on:click="increment()"> Increment</button>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "Increment",
+    data(){
+        return {a:0}
+    },
+    methods:{
+        increment(){
+            this.a++
+          }
+    }
+}
+</script>
